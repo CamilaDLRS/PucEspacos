f75a2c103edb35f21f400ca1f97dfd850003e387
Leosilva1698 <133409205+Leosilva1698@users.noreply.github.com>
2024-05-18 16:18:46 -0300

    feat: adicionado os arquivos de selecao de proposito da reserva e de revisao da reserva

---
+++ Client/src/components/formReservationPurpose/formReservationPurpose.jsx
@@ -0,0 +1,13 @@
+import Filters from "../filters/filters";
+
+function FormResrvationPurpose() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default FormResrvationPurpose;
