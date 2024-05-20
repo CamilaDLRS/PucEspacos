17fc31f5b8f85ef2a594a7b88d58559e7a0f0c69
Dimitri Delinski <dimitrisonet@gmail.com>
2024-05-19 22:58:56 -0300

    feat: card reserva criado dinamicamente

--- Client/src/components/cardReservation/cardReservation.jsx
+++ Client/src/components/cardReservation/cardReservation.jsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react";
 import "./cardReservation.css";
 
-function CardReservation() {
+function CardReservation({reserve}) {
+
+    const [reserveInfos, setReserveInfos ] = useState({
+        day: "",
+        weekDay: "",
+        month: "",
+        year: "",
+        checkin: "",
+        checkout: "",
+        facilityName: reserve.facilityName,
+        requestingUserName: reserve.requestingUserName,
+        responsibleUserName: reserve.responsibleUserName,
+        reservationPurpose: reserve.reservationPurpose,
+        reservationStatus: reserve.reservationStatus
+    })
+
+    useEffect(() => {
+        const checkinDate =  new Date(reserve.checkinDate);
+        const checkoutDate =  new Date(reserve.checkoutDate);
+        const weekDays = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado"];
+        const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+        const day = checkinDate.getDate() < 10 ? "0" + checkinDate.getDate().toString() : checkinDate.getDate();
+        const weekDay = weekDays[checkinDate.getDay()];
+        const month = months[checkinDate.getMonth()] 
+        const year = checkinDate.getFullYear();
+
+        const checkinHour = checkinDate.getHours() < 10 ? "0" + checkinDate.getHours().toString() : checkinDate.getHours();
+        const checkinMinutes = checkinDate.getMinutes() < 10 ? "0" + checkinDate.getMinutes().toString() : checkinDate.getMinutes();
+        const checkin = `${checkinHour}:${checkinMinutes}`;  
+
+        const checkoutHour = checkoutDate.getHours() < 10 ? "0" + checkoutDate.getHours().toString() : checkoutDate.getHours();
+        const checkoutMinutes = checkoutDate.getMinutes() < 10 ? "0" + checkoutDate.getMinutes().toString() : checkoutDate.getMinutes();
+        const checkout = `${checkoutHour}:${checkoutMinutes}`;
+
+        setReserveInfos({...reserveInfos, day: day, weekDay: weekDay, month: month, year: year, checkin: checkin, checkout: checkout});
+    }, [])
+
     return ( 
         <div className="card-reservation">
             <div className="card-reservation-header">
-                <span>20 - Quarta-Feira</span>
-                <span>Março 2024</span>
+                <span>{reserveInfos.day} - {reserveInfos.weekDay}</span>
+                <span>{reserveInfos.month} {reserveInfos.year}</span>
             </div>
             <div  className="card-reservation-body">
                 <table>
@@ -13,24 +50,24 @@ function CardReservation() {
                         <th> Horário Reserva </th>
                         <th> Localização </th>
                         <th> Reservante </th>
-                        {"se tem responsável" === "true" &&
+                        {reserveInfos.responsibleUserName &&
                             <th> Responsavel </th>
                         }
                         <th> Finalidade </th>
                     </tr>
                     <tr>
-                        <td> 07:45 - 10:30 </td>
-                        <td> Araça 303 </td>
-                        <td> -- </td>
-                        {"se tem responsável" === "true" &&
-                            <td> Steve Jobs </td>
+                        <td> {reserveInfos.checkin} - {reserveInfos.checkout} </td>
+                        <td> {reserveInfos.facilityName} </td>
+                        <td> {reserveInfos.requestingUserName} </td>
+                        { reserveInfos.responsibleUserName &&
+                            <td> {reserveInfos.responsibleUserName} </td>
                         }
-                        <td> Estudos </td>
+                        <td> {reserveInfos.reservationPurpose} </td>
                     </tr>
                 </table>
             </div>
             <div className="card-reservation-footer">
-                <span> Solicitada </span>
+                <span> {reserveInfos.reservationStatus} </span>
                 <span> Icones </span>
             </div>
         </div> 
