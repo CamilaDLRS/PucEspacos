4cf33f84e20010c4b6b2a3c9093aa277c3003e04
Dimitri Delinski <dimitrisonet@gmail.com>
2024-06-01 18:22:45 -0300

    algumas responsabilidades kkkk

--- Client/src/components/header/header.jsx
+++ Client/src/components/header/header.jsx
@@ -1,12 +1,13 @@
 import "./header.css"
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import MobileNavbar from "./mobile-navbar.js"
 
 import CardConfirmation from "../cardConfirmation/cardConfirmation";
 
 
 function Header(prop) {
-    const [triggerCardConfirmation, settriggerCardConfirmation] = useState(false);
+    const [mobileNavbar, setMobileNavbar] = useState(new MobileNavbar(".mobile-menu", ".nav-list", ".nav-link"));
 
     useEffect(() => {
         if (localStorage.getItem("userType")) {
@@ -14,18 +15,33 @@ function Header(prop) {
         }
     }, [])
 
+    useEffect(() => {
+        setMobileNavbar(new MobileNavbar( ".mobile-menu", ".nav-list",".nav-link"))
+    }, [])
+    
+    const [triggerCardConfirmation, settriggerCardConfirmation] = useState(false);
     function showCardConfirmation() {
         triggerCardConfirmation ? settriggerCardConfirmation(false) : settriggerCardConfirmation(true)
     }
 
+    mobileNavbar.Init();
+    console.log(mobileNavbar)
     return ( 
         <header>
             <div className="header-title">
-                <h1>Puc Espaços</h1>
+                <h1 className="title">Puc Espaços</h1>
                 <h4>Seja bem-vindo(a) {localStorage.getItem("userName")}</h4>
             </div>
 
-            <nav>
+            <div className="mobile-menu-area">
+                <div className="mobile-menu">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+            </div>
+  
+            <nav className="nav-list">
                 {
                     ((localStorage.getItem("userType") !== "Discente")) && 
                     <Link className="nav-link" to="/reservations" id="reservations"> Reservas </Link>
---
+++ Client/src/components/header/mobile-navbar.js
@@ -0,0 +1,38 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+ 
+    animateLinks() { 
+        console.log(this.navLinks);
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+             ? (link.style.animation = "")
+             : (link.style.animation = `navLinkFade .5s ease forwards ${index / 7  + .3}s`)
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    } 
+
+    addClickEvent() {
+        console.log(this);
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    Init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+    } 
+}
+
+export default MobileNavbar;
